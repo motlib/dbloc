fa5dcd42cbaebba3d703c023b639b88683f3b2c0
Andreas Schroeder <andreas@a-netz.de>
2020-05-22 19:12:29 +0800

    Fix version update script. Fix python requirements file.

--- tools/update-version.sh
+++ tools/update-version.sh
@@ -9,7 +9,7 @@ function set_info {
 
     echo "Setting ${KEY} = '${VAL}'"
 
-    sed -i -e "/^${KEY} = /c ${KEY} = '${VAL}' " dbloc/versioninfo.py
+    sed -i -e "/^${KEY} = /c ${KEY} = '${VAL}' " dbloc_project/versioninfo.py
 }
 
 GIT_HASH=$(git rev-parse HEAD)
